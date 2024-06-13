f5ccf5e7bbb6bbfb041a17b89f83c50db53b8e9e
TahaThinks <tahaa4587@gmail.com>
2024-06-13 22:44:17 +0400

    Positional Arguments

--- StartingFlask/decorator_excercise3.py
+++ StartingFlask/decorator_excercise3.py
@@ -8,7 +8,21 @@ new_user = User("Taha")
 print(new_user.is_logged_in)
 
 
+def is_authenticated_decorator(function):
+    def wrapper(*args):
+        if args[0].is_logged_in:
+            function(args[0])
+        else:
+            print("This user is not Logged in, thus not allowed to create a blog post")
+
+    return wrapper
+
+
+@is_authenticated_decorator
 def create_blog_post(user):
     print(f"This is {user.name}'s new blog post")
 
+
+create_blog_post(new_user)
+new_user.is_logged_in = True
 create_blog_post(new_user)
