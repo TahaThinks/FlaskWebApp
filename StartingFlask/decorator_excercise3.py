6d0de6fe049564bdac7e7d89caf1ff337fac9113
TahaThinks <tahaa4587@gmail.com>
2024-06-13 22:04:57 +0400

    Create a Function

--- StartingFlask/decorator_excercise3.py
+++ StartingFlask/decorator_excercise3.py
@@ -3,3 +3,12 @@ class User:
         self.name = name
         self.is_logged_in = False
 
+
+new_user = User("Taha")
+print(new_user.is_logged_in)
+
+
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post")
+
+create_blog_post(new_user)
