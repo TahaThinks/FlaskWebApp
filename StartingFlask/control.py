a5abe659bc9444dd8660c8323e95e1bd4da734cf
TahaThinks <tahaa4587@gmail.com>
2024-06-13 21:37:22 +0400

    Simple Website Content

--- StartingFlask/control.py
+++ StartingFlask/control.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return ("<h1 style='text-align:center'>Hello, World!</h1>"
+            "<p>This is a paragraph</p>"
+            "<img src='https://hips.hearstapps.com/hmg-prod/images/2023-porsche-911-dakar-122-1668632011.jpg?crop=0.598xw:0.897xh;0.163xw,0.0911xh&resize=1200:*' width='200px'></img>"
+            )
 
 
 @app.route("/bye")
