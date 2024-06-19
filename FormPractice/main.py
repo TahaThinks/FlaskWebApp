f93eafe0f48d21a36d4600cd51d0913b501c648f
TahaThinks <tahaa4587@gmail.com>
2024-06-19 22:01:49 +0400

    Send Form data to Flash APP

--- FormPractice/main.py
+++ FormPractice/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/login",methods = ["POST"])
+def receive_data():
+    name = request.form['name']
+    password = request.form['password']
+    return f'{name} {password}'
 
 if __name__ == '__main__':
     app.run(debug=True)
